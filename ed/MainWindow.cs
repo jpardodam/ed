bf9698b32d70a19b10b6d0beaec06b06e33600b1
jpardodam <1dam.jpardo@institutoserpis.org>
2017-12-12 08:43:40 +0100

    primera subida

---
+++ ed/MainWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+//		Build ();
+//		for (int index = 0; index < 10; index++) {
+//			Button button = new Button ();
+//			button.Label = (index + 1).ToString ();
+//			button.Visible = true;
+//			uint row = (uint)(index / 10);
+//			uint column = (uint)(index % 10);
+//			table.Attach (button, (uint)index, (uint)(index + 1), 0, 1);
+//		}
+//
+		for (uint row = 0; row <9; row++)
+			
+		table.Visible = true;
+		VBoxMain.Add (table);
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+}
