63d02fa5264e943d2c040501287885b7fc8e3c19
jpardodam <1dam.jpardo@institutoserpis.org>
2018-03-06 08:15:19 +0100

    Esta es la primera revisión de ASuma

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View View){
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        startActivity(intent);
+    }
+}
